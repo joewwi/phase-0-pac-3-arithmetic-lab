42fb208f1d9ba388d198a0d3b3b70eb0a1ce8d62
Martin <joewwi03@gmail.com>
2023-03-11 21:22:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+let m = 800;
+let a = 8;
+function add(m,a) {
+    return m + a;
+}
+let w = 30;
+let t = 7;
+function subtract(r,t) {
+    return r - t;
+}
+let i = 4;
+let n = 11799;
+function multiply(i,n) {
+    return i * n;
+}
+let j = 7199.596977329974;
+let o = 9856;
+function divide(j,o) {
+    return j / o;
+}
+let e = 3;
+function increment(e) {
+    ++e;
+    return e;
+}
+let r = 1;
+function decrement(r) {
+    --r;
+    return r;
+}
+let k = "839";
+function makeInt(k) {
+    return parseInt(k, 10);
+}
+let s = "2.222"
+function preserveDecimal(s) {
+    return parseFloat(s);
+}
